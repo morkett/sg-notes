94a1f5f0a2ad1164bf952c869dff467d5f24cc12
morkett <dcorkett@spartaglobal.co>
2017-02-27 15:36:35 +0000

    update js; if/else

--- week03/main.js
+++ week03/main.js
@@ -23,7 +23,7 @@ var number1 = 3;
 var number2 = 7;
 
 console.log(numberAsString + number1);
-console.log(typeof (numberAsString + number1));
+console.log(typeof(numberAsString + number1));
 console.log(typeof(number1 + numberAsString));
 
 console.log(number1 + number2);
@@ -39,3 +39,64 @@ console.log(parseInt(3.1415927));
 console.log(parseInt(3.56));
 // parseFloat - KEEPS TO A REAL (FLOAT) NUMBER
 console.log(parseFloat(3.56));
+
+//IF STATMENTS
+console.log('Control flow:');
+var moneyInPocket = 20;
+
+if (moneyInPocket > 10) {
+    console.log('Another drink please!');
+} else if (moneyInPocket > 5) {
+    console.log('Make mine a half then');
+} else {
+    console.log('Time to go home');
+}
+
+console.log('It\'s all cool.');
+
+////////////////////////////////////////////////////////////SWITCH STATMENTS
+///////////////////////////////////////////////////////////
+var countryOfOrigin = 'France';
+var greeting = '';
+
+switch (countryOfOrigin) {
+    case 'France':
+        greeting = 'Bonjour';
+        break;
+
+    case 'Spain':
+        greeting = 'Hola!';
+        break;
+    case 'Indonesia':
+        greeting = 'Selament sore';
+
+        break;
+    case 'Kenya':
+        greeting = 'Jambo';
+        break;
+    default:
+        greeting = 'Well Hello There';
+        break;
+}
+
+///////////////////////////////////////////////////
+console.log(greeting);
+
+var dayOfWeek = 'friday'
+
+if (moneyInPocket > 10 && dayOfWeek.toUpperCase() === 'FRIDAY') {
+    console.log('Pub thisavo!');
+} else {
+    console.log('Bummer :-(');
+}
+///////////////////////////////////////////////////
+
+console.log(greeting);
+
+var dayOfWeek = 'friday'
+
+if (dayOfWeek.toUpperCase() === 'FRIDAY' || dayOfWeek.toUpperCase() === 'THURSDAY') {
+    console.log('Pub thisavo!');
+} else {
+    console.log('Bummer :-(');
+}
