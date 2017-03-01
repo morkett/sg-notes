5ade8e07c4d597367ef57388f337a6caff4b2ff0
morkett <dcorkett@spartaglobal.co>
2017-03-01 12:39:26 +0000

    added DOM: createElements, select by tag name; traversing the DOM;

---
+++ week03/js-dom-jquery/main.js
@@ -0,0 +1,54 @@
+console.log('executed: main.js \n ');
+
+//HTML as DOM
+
+/*
+ html = {
+  head = {
+    meta: "property",
+    title: "property",
+    script: "property",
+  },
+  body = {
+    p: "property"
+  }
+}
+*/
+
+console.log(document);
+
+//Changing website title
+document.getElementsByTagName('title')[0].innerHTML = 'Title Change';
+
+console.log('\n--- using querySelectorAll\n ');
+
+var selectedListItems = document.querySelectorAll('li.selected');
+
+console.log('selected:',selectedListItems);
+
+for(var i=0; i < selectedListItems.length; i++) {
+  console.log('selected list item style:  ',selectedListItems[i].style );
+  selectedListItems[i].style.color = 'red';
+}
+
+
+console.log('\n--- Create Element\n ');
+
+//var element to create
+var newListItem = document.createElement('li');
+
+// var of what to add
+newListItem.innerHTML = 'in America';
+
+// we are going to call .appendChild on the parent <ul> Element
+//that <ul> element has an id attribute
+
+// now retrieve the DOM element that contains the list items,
+// because we want to call .appendChild() on it.
+// NOTE: we do NOT pass a CSS selector ('#item-list') to the
+// .getElementById() method
+
+// listContainer finds the id of the <ul> element that contains the li
+var listContainer = document.getElementById('item-list');
+
+listContainer.appendChild(newListItem);
