bec6a00451b488540cc1b8d651d54530ddd19612
morkett <dcorkett@spartaglobal.co>
2017-03-15 14:10:45 +0000

    test commit

--- week05-day03-node/express-web-server-mvc/index.js
+++ week05-day03-node/express-web-server-mvc/index.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = require('./config/router');
 var bodyParser = require('body-parser');
 var layouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
 var app = express();
 var port = 3000;
 
+
 app.set('view engine', 'ejs');
+
+
+
 app.use(function (req, res, next) {
   // simple middleware logging
   console.log(req.method, req.path);
@@ -14,6 +19,16 @@ app.use(function (req, res, next) {
 app.use(layouts);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(methodOverride(function (req) {
+  console.log('methodOverride check : req.body:',req.body);
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 app.use(router);
 
