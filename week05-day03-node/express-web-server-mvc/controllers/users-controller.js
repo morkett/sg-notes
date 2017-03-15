6d17f185e89893b0c577524504c00da3c113581f
morkett <dcorkett@spartaglobal.co>
2017-03-15 11:18:10 +0000

    added week5-d03

--- week05-day03-node/express-web-server-mvc/controllers/users-controller.js
+++ week05-day03-node/express-web-server-mvc/controllers/users-controller.js
@@ -30,38 +30,27 @@ function getNextUserId() {
 // Action: index
 function indexUsers(req, res) {
   res.render('users/index', {
-    title: 'User list: ',
+    title: 'User list',
     users: users
   });
-
-
-  // var html = '<h1>List of users</h1>';
-  //
-  // html += '<ul>';
-  // for (var i = 0; i < users.length; i++) {
-  //   html += '<li><a href="/users/' + users[i].id + '">' + users[i].firstName + ' ' + users[i].lastName + ' (' + users[i].email + ')' + '</a></li>';
-  // }
-  // html += '</ul>';
-  // res.status(200).send(html);
 }
 
 // Action: new
 function newUser(req, res) {
   res.render('users/new', {
-
     title: 'New user'
   });
 }
 
 // Action: create
 function createUser(req, res) {
-  var userId = getNextUserId();
   var newUser = {
-    id: userId,
+    id: getNextUserId(),
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email
   };
+
   users.push(newUser);
   res.redirect('/users');
 }
@@ -69,11 +58,9 @@ function createUser(req, res) {
 // Action: edit
 function editUser(req, res) {
   var userId = req.params.id;
-  var userIndex;
+  var userIndex = findUserIndexById(userId);
   var user;
   var status;
-
-  userIndex = findUserIndexById(userId);
 
   if (userIndex !== -1) {
     user = users[userIndex];
@@ -84,30 +71,29 @@ function editUser(req, res) {
 
   res.status(status).render('users/edit', {
     title: 'Edit user ' + userId,
-    userId: userId,
     user: user
   });
 }
 
 // Action: update
 function updateUser(req, res) {
-
   var userId = req.params.id;
-  var user;
-  var html = '<h1>Updating user with id: ' + userId + '</h1>';
   var userIndex = findUserIndexById(userId);
-
+  var user;
+  var json;
 
   if (userIndex !== -1) {
+    // found the user
     user = users[userIndex];
     user.firstName = req.body.firstName;
     user.lastName = req.body.lastName;
     user.email = req.body.email;
-    html += '<p>User updated</p>';
+    // TODO: fix: this produces an error - "Cannot PUT /users"
     res.redirect('/users');
   } else {
-    html += '<em>Could not find user with id ' + userId + '</em>';
-    res.status(404).send(html);
+    // user with :id does not exist
+    json = { error: 'Could not find user with id ' + userId };
+    res.status(404).json(json);
   }
 }
 
@@ -137,23 +123,20 @@ function showUser(req, res) {
 function destroyUser(req, res) {
   var userId = req.params.id;
   var userIndex;
-  // var html = '<h1>Delete user ' + userId + '</h1>';
+  var json;
 
   userIndex = findUserIndexById(userId);
 
   if (userIndex !== -1) {
     // user exists
     users.splice(userIndex, 1);
-
-    // html += 'User with id ' + userId + ' deleted';
-    res.render('users/delete', {
-      title: 'Delete user ' + userId,
-      user: userId
-    });
-  } else {
+    // TODO: fix: this produces an error - "Cannot DELETE /users"
     res.redirect('/users');
+  } else {
+    // trying to delete non-existent user
+    json = { error: 'Could not find user with id ' + userId };
+    res.status(404).json(json);
   }
-
 }
 
 module.exports = {
