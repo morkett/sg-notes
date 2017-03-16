b33bc64929d9bc8b1e6d11e88c6f485822096694
morkett <dcorkett@spartaglobal.co>
2017-03-16 10:31:15 +0000

    changed file names

--- week05-day03-node-mongo/express-web-server-mvc/index.js
+++ week05-day03-node-mongo/express-web-server-mvc/index.js
@@ -3,14 +3,13 @@ var router = require('./config/router');
 var bodyParser = require('body-parser');
 var layouts = require('express-ejs-layouts');
 var methodOverride = require('method-override');
+var mongoose = require('mongoose');
 var app = express();
 var port = 3000;
 
+mongoose.connect('mongodb://localhost/sg-mvc');
 
 app.set('view engine', 'ejs');
-
-
-
 app.use(function (req, res, next) {
   // simple middleware logging
   console.log(req.method, req.path);
@@ -19,9 +18,7 @@ app.use(function (req, res, next) {
 app.use(layouts);
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
 app.use(methodOverride(function (req) {
-  console.log('methodOverride check : req.body:',req.body);
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     // look in urlencoded POST bodies and delete it
     var method = req.body._method;
