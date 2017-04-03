f45cf4739384ac2718d56e5a79c5cb3201eff36c
morkett <dcorkett@spartaglobal.co>
2017-04-03 16:43:18 +0100

    implement route protection

--- week08-day01-angular-authentication/angular-firebase-authentication/public/js/app.js
+++ week08-day01-angular-authentication/angular-firebase-authentication/public/js/app.js
@@ -1,4 +1,4 @@
-function MainRouter ($stateProvider, $urlRouterProvider, $locationProvider) {
+function MainRouter ($stateProvider, $urlRouterProvider, $locationProvider, AuthFactory) {
 
   $stateProvider
     .state('home', {
@@ -13,19 +13,40 @@ function MainRouter ($stateProvider, $urlRouterProvider, $locationProvider) {
       url: '/login',
       templateUrl: '/states/login.html'
     })
-    .state('authRequired', {
+    .state('auth-required', {
       url: '/authrequired',
-      templateUrl: '/states/authRequired.html'
+      templateUrl: '/states/auth-required.html'
     })
     .state('secret', {
       url: '/secret',
-      templateUrl: '/states/secret.html'
+      templateUrl: '/states/secret.html',
+      //resolve - before you go to this state you must resolve whatever is in here
+      resolve: {
+        currentAuth: [
+          'AuthFactory', (AuthFactory) => {
+            return AuthFactory.$requireSignIn();
+          }
+        ]
+      }
     });
 
   $urlRouterProvider.otherwise('/');
 
 }
 
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function AuthCatcher($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', (event, toState, toParams, fromState, fromParams, error) =>{
+    if (error === 'AUTH_REQUIRED') {
+      $state.go('auth-required');
+    }
+  });
+}
+
+// AuthCatcher.$inject = ['$rootScope', '$state'];
+
 angular
   .module('myApp', ['ui.router','firebase'])
-  .config(MainRouter);
+  .config(MainRouter)
+  .run(AuthCatcher);
