e7416e424c425eab3c3eecb9990ce933ac903caa
morkett <dcorkett@spartaglobal.co>
2017-04-19 15:40:00 +0100

    add test for add() method on todos controller

--- week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
+++ week10-day03-angular-testing/unit-testing-angularjs-controller/spec/todos.controller.spec.js
@@ -2,14 +2,23 @@ describe('TodosController', () => {
   // declare a var that can be used to create an instance of the controller for tests
   let controllerToTest;
 
+//declares a var that will be used to create a mock todo factory
+  let MockTodosFactory;
+
 //in all the tests that follow, do eveything thats in here
   beforeEach(() => {
     //name of the app that the controller is going to be connected to
     module('todosApp');
+    MockTodosFactory = {
+      list: [],
+      add: jasmine.createSpy(),
+      clear: jasmine.createSpy()
+    };
     inject(($controller) => {
       //pass $controller method the name of what you want to create see bottom oftodos.controller.js
       // it will create a new controller with this name - create an instance of this controller
-      controllerToTest = $controller('TodosController');
+      // TodosFactory matches the name in todos.controller.js dependencies at the top eg $http.
+      controllerToTest = $controller('TodosController', {TodosFactory: MockTodosFactory });
     });
   });
 
@@ -21,6 +30,31 @@ describe('TodosController', () => {
     it('should initialise inputText correctly', () => {
       //.inputText matches .inputText in todos.controller.js
       expect(controllerToTest.inputText).toEqual('');
+    });
+  });
+
+  describe('add()', () => {
+    it('should call TodosFactory.add() with corret parameter', () => {
+      const testInputText = 'new todo';
+      //primed the controller with a value for input text
+      controllerToTest.inputText = testInputText;
+      //to test this we need to execute it
+      controllerToTest.add();
+      //testing to se if the controllerToTest.add is being called with MockTodosFactory
+      //jasmine.createSpy allows us to put in the inputText
+      expect(MockTodosFactory.add).toHaveBeenCalledWith(testInputText);
+    });
+    it('should clear inputText', () => {
+      it('should call TodosFactory.add() with corret parameter', () => {
+        const testInputText = 'new todo 2';
+
+        controllerToTest.inputText = testInputText;
+        controllerToTest.add();
+        expect(controllerToTest.inputText).toEqual('');
+      });
+
+  // describe('clear()', () =>{
+  //   it('call the TodosFactory.clear() with the correct')
     });
   });
 });
